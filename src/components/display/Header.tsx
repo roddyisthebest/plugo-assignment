47089433bc576836ae318de49041a095d4a1ea67
roddyisthebest <bsy17171@naver.com>
2023-05-26 18:06:50 +0900

    #2 implement Header component

---
+++ src/components/display/Header.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  height: 100px;
+  padding: 0 50px;
+  display: flex;
+  align-items: center;
+  gap: 0 100px;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  color: rgba(0, 0, 0, 0.8);
+  font-weight: 600;
+`;
+
+const Routes = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0 20px;
+`;
+
+export default function Header({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Routes>{children}</Routes>
+    </Container>
+  );
+}
--- src/router/Admin.tsx
+++ src/router/Admin.tsx
@@ -1,3 +1,35 @@
+import styled from 'styled-components';
+import Header from '../components/display/Header';
+
+const Container = styled.div`
+  min-height: 100vh;
+`;
+
+const Button = styled.button`
+  padding: 0 20px;
+  border-radius: 8px;
+  height: 50px;
+  font-size: 15px;
+  font-weight: 600;
+  color: rgba(0, 0, 0, 0.8);
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+
+  transition: all 300ms ease;
+
+  &:hover {
+    background-color: #e1e1e1;
+  }
+`;
+
 export default function Admin() {
-  return <div></div>;
+  return (
+    <Container>
+      <Header title="ADMIN">
+        <Button>CREATE PRODUCT</Button>
+        <Button>USER</Button>
+      </Header>
+    </Container>
+  );
 }
